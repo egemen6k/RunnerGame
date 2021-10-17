9537b6ade31d6602441fad4f3928984b0df9b93e
egemen6k <egemen.altikardesler@gmail.com>
2021-10-17 13:52:03 +0300

    +Updates made

---
+++ RunnerGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public float jumpForce;
+    public float gravityModifier;
+    private bool _isOnGround = true;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        if (_rb == null)
+        {
+            Debug.Log("RB is null");
+        }
+
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
+        {
+            _rb.AddForce(Vector3.up * jumpForce , ForceMode.Impulse);
+            _isOnGround = false;
+        }   
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _isOnGround = true;
+    }
+}
---
+++ RunnerGame/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 _spawnPosition = new Vector3(25f, 0f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obstaclePrefab, _spawnPosition, obstaclePrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
